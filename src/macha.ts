0f7ef004ffbb2a6d2524aab14442ddbc5a8be736
nksajwani <narendra@macha.ai>
2023-12-10 03:29:09 +0530

    remove node_modules

--- src/macha.ts
+++ src/macha.ts
@@ -1,5 +1,5 @@
-import axios from "axios"; 
-import {MongoClient} from "mongodb"; 
+// import axios from "axios"; 
+// import {MongoClient} from "mongodb"; 
 import {
   nftMinted as nftMintedEvent
 } from "../generated/Macha/Macha"
@@ -7,21 +7,21 @@ import {
   nftMinted
 } from "../generated/schema"
 
-const mongoUri = 'mongodb+srv://doshivarun202:KZajssIpBskVfU4M@cluster0.fors5eb.mongodb.net/'; 
-const mongoClient = new MongoClient(mongoUri); 
+// const mongoUri = 'mongodb+srv://doshivarun202:KZajssIpBskVfU4M@cluster0.fors5eb.mongodb.net/'; 
+// const mongoClient = new MongoClient(mongoUri); 
 
-export async function handlenftMinted(event: nftMintedEvent): void {
+export function handlenftMinted(event: nftMintedEvent): void {
   let entity = new nftMinted(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
   entity.uri = event.params.uri.toString()
-  try { 
-    const metadata = await getMetadataUrl(event.params.uri.toString()); 
-    await saveDocumentToMongoDB(metadata, 'metadata'); 
-  } catch (error) { 
-    console.error(error); 
-    throw new Error('Failed to process metadata'); 
-  } 
+  // try { 
+  //   const metadata = await getMetadataUrl(event.params.uri.toString()); 
+  //   await saveDocumentToMongoDB(metadata, 'metadata'); 
+  // } catch (error) { 
+  //   console.error(error); 
+  //   throw new Error('Failed to process metadata'); 
+  // } 
   entity.blockNumber = event.block.number
   entity.blockTimestamp = event.block.timestamp
   entity.transactionHash = event.transaction.hash
@@ -29,33 +29,33 @@ export async function handlenftMinted(event: nftMintedEvent): void {
   entity.save()
 }
 
-async function getMetadataUrl(metadataUri: string): void { 
-  let metadataUrl: string | undefined; 
+// async function getMetadataUrl(metadataUri: string): void { 
+//   let metadataUrl: string | undefined; 
  
-  if (metadataUri.startsWith("ipfs://")) { 
-    metadataUrl = `https://ipfs.io/ipfs/${metadataUri.split("ipfs://")[1]}`; 
-  } else if (metadataUri.startsWith("https://")) { 
-    metadataUrl = metadataUri; 
-  } 
+//   if (metadataUri.startsWith("ipfs://")) { 
+//     metadataUrl = `https://ipfs.io/ipfs/${metadataUri.split("ipfs://")[1]}`; 
+//   } else if (metadataUri.startsWith("https://")) { 
+//     metadataUrl = metadataUri; 
+//   } 
  
-  if (!metadataUrl) { 
-    throw new Error('Invalid metadata URL'); 
-  } 
+//   if (!metadataUrl) { 
+//     throw new Error('Invalid metadata URL'); 
+//   } 
  
-  const metadata = await axios.get(metadataUrl as string); 
-  return metadata; 
-} 
+//   const metadata = await axios.get(metadataUrl as string); 
+//   return metadata; 
+// } 
 
-async function saveDocumentToMongoDB(jsonDocument: any, collectionName: string): void { 
-  try { 
-    await mongoClient.connect(); 
-    console.log('Connected to MongoDB'); 
-    const database = mongoClient.db('denfo'); 
-    const collection = database.collection(collectionName); 
-    const result = await collection.insertOne(jsonDocument); 
-    console.log(`Document inserted with _id: ${result.insertedId}`); 
-  } finally { 
-    await mongoClient.close(); 
-    console.log('Connection to MongoDB closed'); 
-  } 
-} 
+// async function saveDocumentToMongoDB(jsonDocument: any, collectionName: string): void { 
+//   try { 
+//     await mongoClient.connect(); 
+//     console.log('Connected to MongoDB'); 
+//     const database = mongoClient.db('denfo'); 
+//     const collection = database.collection(collectionName); 
+//     const result = await collection.insertOne(jsonDocument); 
+//     console.log(`Document inserted with _id: ${result.insertedId}`); 
+//   } finally { 
+//     await mongoClient.close(); 
+//     console.log('Connection to MongoDB closed'); 
+//   } 
+// } 
